cefc22147b42df98a5480c7e73cfa185812dbb27
HtetMoePhyu <htetmoephyu.mhs@gmail.com>
2021-11-11 10:09:44 +0630

    Initial Commit

---
+++ TabBar/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TabBar
+//
+//  Created by Htet Moe Phyu on 11/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TabBar/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TabBar
+//
+//  Created by Htet Moe Phyu on 11/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
